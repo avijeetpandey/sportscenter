3bb97297d8e5466986c62311c357d25ae58fd5e3
Avijeet Pandey <40532869+avijeetpandey@users.noreply.github.com>
2024-10-11 23:25:17 +0530

    add: specification to product controller (#7)

--- src/main/java/com/ecommerce/sportscenter/service/ProductService.java
+++ src/main/java/com/ecommerce/sportscenter/service/ProductService.java
@@ -4,9 +4,7 @@ import com.ecommerce.sportscenter.model.ProductResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.List;
-
 public interface ProductService {
     ProductResponse getProductById(Integer productId);
-    Page<ProductResponse> getAllProducts(Pageable pageable);
+    Page<ProductResponse> getAllProducts(Pageable pageable, Integer brandId, Integer typeId, String keyword);
 }
