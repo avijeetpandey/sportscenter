aecf2870a259529a960fa621daec80ea6bcfef2b
Avijeet <avijeetpandey25@gmail.com>
2024-10-11 23:35:52 +0530

    remove: unused imports

--- src/main/java/com/ecommerce/sportscenter/controller/ProductController.java
+++ src/main/java/com/ecommerce/sportscenter/controller/ProductController.java
@@ -1,6 +1,5 @@
 package com.ecommerce.sportscenter.controller;
 
-import com.ecommerce.sportscenter.entity.Product;
 import com.ecommerce.sportscenter.model.BrandResponse;
 import com.ecommerce.sportscenter.model.ProductResponse;
 import com.ecommerce.sportscenter.model.TypeResponse;
@@ -10,7 +9,6 @@ import com.ecommerce.sportscenter.service.TypeService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
