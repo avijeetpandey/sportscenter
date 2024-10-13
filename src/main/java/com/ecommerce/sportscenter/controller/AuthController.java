ab12cd951480ba71d6e9df343aef83d1af409bd0
Avijeet <avijeetpandey25@gmail.com>
2024-10-13 12:46:25 +0530

    add: auth controller

---
+++ src/main/java/com/ecommerce/sportscenter/controller/AuthController.java
@@ -0,0 +1,68 @@
+package com.ecommerce.sportscenter.controller;
+
+import com.ecommerce.sportscenter.model.JwtRequest;
+import com.ecommerce.sportscenter.model.JwtResponse;
+import com.ecommerce.sportscenter.security.JwtHelper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    private final UserDetailsService userDetailsService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtHelper jwtHelper;
+
+    public AuthController(UserDetailsService userDetailsService, AuthenticationManager authenticationManager, JwtHelper jwtHelper) {
+        this.userDetailsService = userDetailsService;
+        this.authenticationManager = authenticationManager;
+        this.jwtHelper = jwtHelper;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<JwtResponse> login(@RequestBody JwtRequest request) {
+        this.authenticate(request.getUsername(), request.getPassword());
+        UserDetails userDetails = userDetailsService.loadUserByUsername(request.getUsername());
+        String token = jwtHelper.generateToken(userDetails);
+        JwtResponse jwtResponse = JwtResponse
+                                        .builder()
+                                        .username(userDetails.getUsername())
+                                        .token(token)
+                                        .build();
+        return new ResponseEntity<>(jwtResponse, HttpStatus.OK);
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity<UserDetails> getUserDetails(@RequestHeader("Authorization") String tokenHeader) {
+        String token = extractTokenFromHeader(tokenHeader);
+        if(token != null) {
+            String username = jwtHelper.getUserNameFromToken(token);
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+            return new ResponseEntity<>(userDetails, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    private String extractTokenFromHeader(String tokenHeader) {
+        if(tokenHeader != null && tokenHeader.startsWith("Bearer ")) {
+            return tokenHeader.substring(7);
+        }
+        return null;
+    }
+
+    private void authenticate(String username, String password) {
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
+        try {
+            authenticationManager.authenticate(authenticationToken);
+        } catch (BadCredentialsException e) {
+            throw new BadCredentialsException("Invalid username or password");
+        }
+    }
+}
--- src/main/java/com/ecommerce/sportscenter/controller/ProductController.java
+++ src/main/java/com/ecommerce/sportscenter/controller/ProductController.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -35,6 +36,7 @@ public class ProductController {
     }
 
     @GetMapping()
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<Page<ProductResponse>> getProducts(
             @RequestParam(name = "page" , defaultValue = "0") int page,
             @RequestParam(name = "size" , defaultValue = "10") int size,
